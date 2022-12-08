d832ffb47aee1397f6988fbbf6e0ddd7ac5339eb
angel.samano <angel.samano@softwareone.com>
2022-12-08 17:00:49 +0100

    configure and deploy cdk

--- lib/aws_typescript-stack.ts
+++ lib/aws_typescript-stack.ts
@@ -1,10 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Duration } from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsTypescriptStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new Bucket(this, "awsTypescriptBucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(5),
+        },
+      ],
+    });
 
     // The code that defines your stack goes here
 
