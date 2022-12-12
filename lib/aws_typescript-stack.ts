de4ea84cfdd6433dc0fb3708d2a39438fea4d326
angel.samano <angel.samano@softwareone.com>
2022-12-12 17:06:01 +0100

    Add ts

--- lib/aws_typescript-stack.ts
+++ lib/aws_typescript-stack.ts
@@ -1,19 +1,29 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Bucket } from "aws-cdk-lib/aws-s3";
-import { Duration } from "aws-cdk-lib";
+import { CfnOutput, Duration, CfnParameter } from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsTypescriptStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, "awsTypescriptBucket", {
+    const duration = new CfnParameter(this, "duration", {
+      type: "Number",
+      default: 6,
+      maxValue: 10,
+      minValue: 1,
+    });
+    const myBucket = new Bucket(this, "awsTypescriptBucket", {
       lifecycleRules: [
         {
-          expiration: Duration.days(5),
+          expiration: Duration.days(duration.valueAsNumber),
         },
       ],
+    });
+
+    new CfnOutput(this, "my-bucket-angel", {
+      value: myBucket.bucketName,
     });
 
     // The code that defines your stack goes here
